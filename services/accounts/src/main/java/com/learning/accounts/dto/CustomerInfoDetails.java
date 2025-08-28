11d7aaba2b8e4cbbb3439b9c25912e5d4dc6572c
Komal Jaiswal <KomalJaiswal@fico.com>
2025-08-28 17:13:16 +0530

    disabling pull force false to test /monitor and git webhook is actually working

--- services/accounts/src/main/java/com/learning/accounts/dto/CustomerInfoDetails.java
+++ services/accounts/src/main/java/com/learning/accounts/dto/CustomerInfoDetails.java
@@ -9,19 +9,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@ConfigurationProperties(prefix = "accounts")
-@Getter
-@Setter
 /**
  * records create final properties and object is immutable
  * since we have to update config properties at runtime, which we are fetching from spring cloud config server
- * we need to set peropery in object which cant be done using record
+ * we need to set property in object which cant be done using record
  * hence making it simple class
  */
-
-
 //public record CustomerInfoDetails(String message, Map<String,String> contactDetails, List<String> onCallSupport) {
 //}
+@ConfigurationProperties(prefix = "accounts")
+@Getter
+@Setter
 public class CustomerInfoDetails {
     private Map<String, String> contactDetails = new HashMap<>();
     private String message;
