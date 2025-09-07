a36354128567048a2ae2bc840810177616c334e7
Komal Jaiswal <KomalJaiswal@fico.com>
2025-09-07 19:42:02 +0530

    Feign client changes in Account MS ro invoke other MS's

--- services/accounts/src/main/java/com/learning/accounts/AccountsApplication.java
+++ services/accounts/src/main/java/com/learning/accounts/AccountsApplication.java
@@ -2,13 +2,14 @@ package com.learning.accounts;
 
 import com.learning.accounts.dto.CustomerInfoDetails;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 @EnableConfigurationProperties(value = {CustomerInfoDetails.class})
+@EnableFeignClients
 public class AccountsApplication {
 
     public static void main(String[] args) {
