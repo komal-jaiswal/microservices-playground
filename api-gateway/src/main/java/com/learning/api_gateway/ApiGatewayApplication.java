4eb64933866040ec7dd7ed50f314af19d971e2c4
Komal Jaiswal <KomalJaiswal@fico.com>
2025-09-30 13:40:39 +0530

    API gateway implementation with custom filter

--- api-gateway/src/main/java/com/learning/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/learning/api_gateway/ApiGatewayApplication.java
@@ -2,12 +2,32 @@ package com.learning.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 
+    @Bean
+    public RouteLocator routeConfig(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p
+                        .path("/learning/accounts/**")
+                        .filters(f -> f.rewritePath("/learning/accounts/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://ACCOUNTS"))
+                .route(p -> p
+                        .path("/learning/loans/**")
+                        .filters(f -> f.rewritePath("/learning/loans/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://LOANS"))
+                .route(p -> p
+                        .path("/learning/cards/**")
+                        .filters(f -> f.rewritePath("/learning/cards/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://CARDS"))
+                .build();
+    }
 }
